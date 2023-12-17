43cff20f604193b4b7a0481a670342ef3f17974c
Manikya-Sharma <92919520+Manikya-Sharma@users.noreply.github.com>
2023-12-17 18:35:29 +0530

    explore page with masonry

--- src/app/(logged-in)/explore/page.tsx
+++ src/app/(logged-in)/explore/page.tsx
@@ -2,6 +2,9 @@
 
 import PostPreview from "@/components/PostPreview";
 import DoubtPreview from "@/components/DoubtPreview";
+
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+
 import { Skeleton } from "@/components/ui/skeleton";
 import { Post } from "@/types/post";
 import { Account } from "@/types/user";
@@ -16,6 +19,28 @@ const Page = () => {
   const [posts, setPosts] = useState<Array<Post>>([]);
   const [doubts, setDoubts] = useState<Array<Doubt>>([]);
   const [authors, setAuthors] = useState<Map<string, string> | null>(null);
+
+  const results: Array<Post | Doubt> = [];
+
+  // shuffle results
+  for (let i = 0; i < Math.max(posts.length, doubts.length); i += 1) {
+    if (i % 3 == 0) {
+      if (i < doubts.length) {
+        results.push(doubts[i]);
+      }
+      if (i < posts.length) {
+        results.push(posts[i]);
+      }
+    } else {
+      if (i < posts.length) {
+        results.push(posts[i]);
+      }
+      if (i < doubts.length) {
+        results.push(doubts[i]);
+      }
+    }
+  }
+
   const loading = posts.length === 0;
 
   useEffect(() => {
@@ -96,32 +121,38 @@ const Page = () => {
   }
 
   return (
-    <div className="flex flex-col gap-7">
-      <div className="lg:flex lg:justify-center lg:gap-7">
-        <div className="mb-7 flex flex-col gap-5 lg:mb-0 lg:gap-7">
-          {posts.map((post) => {
-            return (
-              <PostPreview key={post.id} post={post} authorMap={authors} />
-            );
+    <div className="mx-auto max-w-6xl">
+      <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2 }}>
+        <Masonry gutter="16px">
+          {results.map((res) => {
+            if (Object.hasOwn(res, "coverImgFull")) {
+              return (
+                //@ts-ignore
+                <PostPreview key={res.id} post={res} authorMap={authors} />
+              );
+            } else {
+              return (
+                //@ts-ignore
+                <DoubtPreview key={res.id} doubt={res} authorMap={authors} />
+              );
+            }
           })}
-        </div>
-
-        <div className="flex flex-col gap-5 lg:gap-7">
-          {doubts.map((doubt) => {
-            return (
-              <DoubtPreview key={doubt.id} doubt={doubt} authorMap={authors} />
-            );
-          })}
-        </div>
-      </div>
+        </Masonry>
+      </ResponsiveMasonry>
 
       {loading && (
-        <>
-          <Skeleton className="mx-auto aspect-video w-[80%] max-w-prose" />
-          <Skeleton className="mx-auto aspect-video w-[80%] max-w-prose" />
-          <Skeleton className="mx-auto aspect-video w-[80%] max-w-prose" />
-          <Skeleton className="mx-auto aspect-video w-[80%] max-w-prose" />
-        </>
+        <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2 }}>
+          <Masonry gutter="10px">
+            <Skeleton className="mx-auto h-60 w-[80%] max-w-prose" />
+            <Skeleton className="mx-auto aspect-video w-[80%] max-w-prose" />
+            <Skeleton className="mx-auto aspect-video w-[80%] max-w-prose" />
+            <Skeleton className="mx-auto aspect-video w-[80%] max-w-prose" />
+            <Skeleton className="mx-auto h-60 w-[80%] max-w-prose" />
+            <Skeleton className="mx-auto h-60 w-[80%] max-w-prose" />
+            <Skeleton className="mx-auto aspect-video w-[80%] max-w-prose" />
+            <Skeleton className="mx-auto aspect-video w-[80%] max-w-prose" />
+          </Masonry>
+        </ResponsiveMasonry>
       )}
     </div>
   );
